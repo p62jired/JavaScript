4b1105d6dfbd5b2337bc915f9011b219c1ccfc33
p62jired <66884280+p62jired@users.noreply.github.com>
2022-01-17 13:34:35 +0100

    Fix 2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,42 +2,42 @@ import React from 'react'
 /**
  * Los corchetes toman la propiedad de un objeto directamente.
  * Es decir const {text} = props
-const Title = ({text}) => <h1> {text}</h1>
+const Header = ({text}) => <h1> {text}</h1>
  */
-const Title = (props) => {
+const Header = (props) => {
   return <h1> {props.text}</h1>
 }
-const Tema = ({text}) => <h2> {text}</h2>
+const Content = ({p1,p2,p3}) => {  
+return (
+    <div>
+      <h1>Part 1:  {p1}</h1>
+      <h1>Part 2:  {p2}</h1>
+      <h1>Part 3:  {p3}</h1>
+    </div>
+  )
+}
+const Total = ({text}) => <h2>{text}</h2>
 
-const Ejercicios = ({text}) => <h2> Numero de ejercicios {text}</h2>
 
-const Motivo = ({text}) => <h3>{text}</h3>
 
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
-  const part2 = 'Using props to pass datamod'
+  const part2 = 'Using props to pass data'
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
 
   return (
     <div>
-      <Title text={course}/>
+      <Header text={course}/>
       <p>
-      <Tema text={part1}/>
+      <Content p1={part1} p2={part2} p3={part3}/>
       </p>
       <p>
-      <Ejercicios text={exercises1 + exercises2}/>
+      <Total text={exercises1 + exercises2 + exercises3}/>
       </p>
-      <p>
-      <Motivo text={part2}/>
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
 }
